f1076a70942085e6413d1e3c89322ddd677dd592
Vasos <vasos.games@gmail.com>
2021-02-12 17:16:57 +0300

    Добавил форму

--- js/script.js
+++ js/script.js
@@ -6,12 +6,7 @@ subToggle.onclick = () => {
     console.log('f');
     subMenu.classList.toggle('submenu--active')
 }
-// subToggle.onclick = () => {
-//     subMenu.toggle;
-// }
 
-// JQuery(() => {
-//     $('#subToggle').onclick = () => {
-//         $('.submenu').toggle();
-//     }
-// })
+let tel = document.querySelector('input[type="tel"]')
+let mask = new Inputmask('+7( 999 ) 999 - 99 - 99');
+mask.mask(tel);
